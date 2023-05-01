876bdd3c6cd16dfec0e914d9ff024d6a1b67c74a
m <phamichael123@gmail.com>
2023-05-01 20:34:58 +0200

    Ajout du main

---
+++ src/com/mph/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.mph;
+
+public class HelloWorld 
+{
+	
+	public static void main(String[] args) 
+	{
+		sayHello();
+	}
+	
+	private static void sayHello() 
+	{
+		System.out.println("Hello World!");
+	}
+
+}
